ccea8d9ac71fb735fb199c29b0b803d7c91149ab
manuc66 <manuc66@gmail.com>
2017-06-20 19:33:30 +0200

    move tests

--- JsonSubTypes.Tests/DiscriminatorOfDifferentKindTests.cs
+++ JsonSubTypes.Tests/DiscriminatorOfDifferentKindTests.cs
@@ -8,7 +8,7 @@ namespace JsonSubTypes.Tests
     public class DiscriminatorOfDifferentKindTests
     {
         [TestClass]
-        public class DiscriminatorAsAnEnum
+        public class DiscriminatorIsAnEnum
         {
             public class MainClass
             {
@@ -44,6 +44,49 @@ namespace JsonSubTypes.Tests
             {
                 var obj = JsonConvert.DeserializeObject<MainClass>("{\"SubTypeData\":{\"ZzzField\":\"zzz\",\"SubTypeType\":1}}");
                 Assert.AreEqual("zzz", (obj.SubTypeData as SubTypeClass2)?.ZzzField);
+            }
+        }
+
+        [TestClass]
+        public class DiscriminatorIsAnInt
+        {
+            class Parent
+            {
+                public Child child { get; set; }
+            }
+
+            [JsonConverter(typeof(JsonSubtypes), "ChildType")]
+            [JsonSubtypes.KnownSubType(typeof(Child1), 1)]
+            [JsonSubtypes.KnownSubType(typeof(Child2), 2)]
+            class Child
+            {
+                public virtual int ChildType { get; }
+            }
+
+            class Child1 : Child
+            {
+                public override int ChildType { get; } = 1;
+            }
+
+            class Child2 : Child
+            {
+                public override int ChildType { get; } = 2;
+            }
+
+            [TestMethod]
+            public void DiscriminatorValueCanBeANumber()
+            {
+                var root1 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":1}}");
+                var root2 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":2}}");
+                var root3 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":8}}");
+                var root4 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":null}}");
+                var root5 = JsonConvert.DeserializeObject<Parent>("{\"child\":{}}");
+
+                Assert.IsNotNull(root1.child as Child1);
+                Assert.IsNotNull(root2.child as Child2);
+                Assert.AreEqual(typeof(Child), root3.child.GetType());
+                Assert.AreEqual(typeof(Child), root4.child.GetType());
+                Assert.AreEqual(typeof(Child), root5.child.GetType());
             }
         }
     }
--- JsonSubTypes.Tests/JsonSubTypesTests.cs
+++ JsonSubTypes.Tests/JsonSubTypesTests.cs
@@ -214,42 +214,6 @@ namespace JsonSubTypes.Tests
             Assert.AreEqual(expected, root);
         }
 
-        class Parent
-        {
-            public Child child { get; set; }
-        }
-
-        [JsonConverter(typeof(JsonSubtypes), "ChildType")]
-        [JsonSubtypes.KnownSubType(typeof(Child1), 1)]
-        [JsonSubtypes.KnownSubType(typeof(Child2), 2)]
-        class Child
-        {
-            public virtual int ChildType { get; }
-        }
-
-        class Child1 : Child
-        {
-            public override int ChildType { get; } = 1;
-        }
-        class Child2 : Child
-        {
-            public override int ChildType { get; } = 2;
-        }
-
-        [TestMethod]
-        public void DiscriminatorValueCanBeANumber()
-        {
-            var root1 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":1}}");
-            var root2 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":2}}");
-            var root3 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":8}}");
-            var root4 = JsonConvert.DeserializeObject<Parent>("{\"child\":{\"ChildType\":null}}");
-            var root5 = JsonConvert.DeserializeObject<Parent>("{\"child\":{}}");
-
-            Assert.IsNotNull(root1.child as Child1);
-            Assert.IsNotNull(root2.child as Child2);
-            Assert.AreEqual(typeof(Child), root3.child.GetType());
-            Assert.AreEqual(typeof(Child), root4.child.GetType());
-            Assert.AreEqual(typeof(Child), root5.child.GetType());
-        }
+     
     }
 }
