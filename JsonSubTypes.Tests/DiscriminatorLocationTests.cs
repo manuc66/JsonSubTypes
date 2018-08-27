08578435c008f1efb4e644e2bd815f46753b11e7
robertsi <robertsi@PC6444>
2018-08-27 14:08:23 -0400

    Add some test cases
    check that the discriminator works when the property is output first for deserialization.
    
    also check that the flag appropriately forces the discriminator to the first property.
    
    the default "false" case is already handled by the other test cases.

---
+++ JsonSubTypes.Tests/DiscriminatorLocationTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace JsonSubTypes.Tests
+{
+
+    class SimpleBase
+    {
+        public String Name { get; set; } 
+    }
+
+    class SimpleChildA : SimpleBase
+    {
+        public int Age { get; set; }
+    }
+
+    class SimpleChildB : SimpleBase
+    {
+        public int Height { get; set; }
+    }
+
+    [TestFixture]
+    public class DiscriminatorLocationTests
+    {
+        [Test]
+        public void CheckFirst()
+        {
+            var settings = new JsonSerializerSettings();
+            JsonConvert.DefaultSettings = () => settings;
+
+            settings.Converters.Add(JsonSubtypesConverterBuilder
+                .Of(typeof(SimpleBase), "type")
+                .SerializeDiscriminatorProperty(true)
+                .RegisterSubtype(typeof(SimpleChildA), "TypeA")
+                .RegisterSubtype(typeof(SimpleChildB), "TypeB")
+                .Build());
+
+
+            SimpleBase test_object = new SimpleChildA() { Name = "bob", Age = 12 };
+
+
+            var json_first = "{\"type\":\"TypeA\",\"Age\":12,\"Name\":\"bob\"}";
+
+            var result = JsonConvert.SerializeObject(test_object);
+            
+            Assert.AreEqual(json_first, result);
+        }
+
+        [Test]
+        public void checkDeserializeFirst()
+        {
+            var settings = new JsonSerializerSettings();
+            JsonConvert.DefaultSettings = () => settings;
+
+            settings.Converters.Add(JsonSubtypesConverterBuilder
+                .Of(typeof(SimpleBase), "type")
+                .SerializeDiscriminatorProperty(true)
+                .RegisterSubtype(typeof(SimpleChildA), "TypeA")
+                .RegisterSubtype(typeof(SimpleChildB), "TypeB")
+                .Build());
+
+
+            SimpleBase test_object = new SimpleChildB() { Name = "bob", Height = 36 };
+
+            var json_first = "{\"type\":\"TypeB\",\"Height\":36,\"Name\":\"bob\"}";
+
+            var result = JsonConvert.DeserializeObject<SimpleBase>(json_first);
+
+            Assert.IsInstanceOf(typeof(SimpleChildB), result);
+
+        }
+    }
+}
