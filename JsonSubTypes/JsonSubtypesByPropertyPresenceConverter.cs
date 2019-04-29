c383021f1fbb78b84e28bb8c3b9a5e08b3cf8fec
Emmanuel Counasse <manuc66@gmail.com>
2019-04-29 23:49:47 +0200

    add converter and builder

---
+++ JsonSubTypes/JsonSubtypesByPropertyPresenceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonSubTypes
+{
+    internal class JsonSubtypesByPropertyPresenceConverter : JsonSubtypesConverter
+    {
+        private readonly Dictionary<string, Type> _jsonPropertyName2Type;
+
+        internal JsonSubtypesByPropertyPresenceConverter(Type baseType, Dictionary<string, Type> jsonProperty2Type) : base(baseType)
+        {
+            _jsonPropertyName2Type = jsonProperty2Type;
+        }
+
+        protected override Dictionary<object, Type> GetSubTypeMapping(Type type)
+        {
+            return new Dictionary<object, Type>();
+        }
+
+        internal override Dictionary<string, Type> GetTypesByPropertyPresence(Type parentType)
+        {
+            return _jsonPropertyName2Type;
+        }
+    }
+}
---
+++ JsonSubTypes/JsonSubtypesWithPropertyConverterBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace JsonSubTypes
+{
+    public class JsonSubtypesWithPropertyConverterBuilder
+    {
+        private readonly Type _baseType;
+        private readonly Dictionary<string, Type> _subTypeMapping = new Dictionary<string, Type>();
+
+        private JsonSubtypesWithPropertyConverterBuilder(Type baseType)
+        {
+            _baseType = baseType;
+        }
+
+        public static JsonSubtypesWithPropertyConverterBuilder Of(Type baseType)
+        {
+            return new JsonSubtypesWithPropertyConverterBuilder(baseType);
+        }
+
+        public JsonSubtypesWithPropertyConverterBuilder RegisterSubtypeWithProperty(Type subtype, string jsonPropertyName)
+        {
+            _subTypeMapping.Add(jsonPropertyName, subtype);
+            return this;
+        }
+
+        public JsonConverter Build()
+        {
+            return new JsonSubtypesByPropertyPresenceConverter(_baseType, _subTypeMapping);
+        }
+    }
+}
