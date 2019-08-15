db13af852f2b8467289d9893303804d5e83c9c82
Jacob Bundgaard <jbu@templafy.com>
2019-08-15 14:01:15 +0200

    Add failing test for infinite loop when deserializing with abstract base class name as discriminator.

---
+++ JsonSubTypes.Tests/AbstractBaseClassDiscriminatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace JsonSubTypes.Tests
+{
+    [TestFixture]
+    public class AbstractBaseClassDiscriminatorTests
+    {
+        [JsonConverter(typeof(JsonSubtypes), nameof(SubTypeClass.Discriminator))]
+        public abstract class MainClass
+        {
+        }
+
+        public class SubTypeClass : MainClass
+        {
+            public string Discriminator => "SubTypeClass";
+        }
+
+        [Test]
+        [Timeout(2000)]
+        public void DeserializingWithAbstractBaseClassDiscriminatorThrows()
+        {
+            Assert.Throws<JsonSerializationException>(() =>
+                JsonConvert.DeserializeObject<MainClass>("{\"Discriminator\":\"MainClass\"}"));
+        }
+    }
+}
