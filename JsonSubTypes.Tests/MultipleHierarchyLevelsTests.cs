6228de62369ae53f1633357534bea22fc3617baa
Emmanuel Counasse <manuc66@gmail.com>
2019-08-18 01:05:23 +0200

    improve tests

--- JsonSubTypes.Tests/MultipleHierarchyLevelsTests.cs
+++ JsonSubTypes.Tests/MultipleHierarchyLevelsTests.cs
@@ -6,15 +6,21 @@ namespace JsonSubTypes.Tests
     [TestFixture]
     public class MultipleHierarchyLevelsTests
     {
-
         [Test]
         public void ShouldDeserializeNestedLevel()
         {
-            Payload run = new Run();
-            var data = JsonConvert.SerializeObject(run);
+            var data = "{\"$GameKind\":0,\"$PayloadKind\":1}";
             Assert.IsInstanceOf<Run>(JsonConvert.DeserializeObject<Payload>(data));
         }
-        
+
+        [Test]
+        public void ShouldSerializeNestedLevel()
+        {
+            Payload run = new Run();
+            var data = JsonConvert.SerializeObject(run);
+            Assert.AreEqual("{\"$GameKind\":0,\"$PayloadKind\":1}", data);
+        }
+
         public enum PayloadDiscriminator
         {
             COM = 0,
@@ -37,7 +43,7 @@ namespace JsonSubTypes.Tests
             [JsonProperty(PAYLOAD_KIND)]
             protected abstract PayloadDiscriminator PayloadKind { get; }
         }
-        
+
         [JsonConverter(typeof(JsonSubtypes), GAME_KIND)]
         [JsonSubtypes.KnownSubType(typeof(Run), GameDiscriminator.RUN)]
         [JsonSubtypes.KnownSubType(typeof(Walk), GameDiscriminator.WALK)]
@@ -70,30 +76,39 @@ namespace JsonSubTypes.Tests
     [TestFixture]
     public class MultipleHierarchyLevelsDynamicRegistrationTests
     {
-
-        [Test]
-        public void ShouldDeserializeNestedLevel()
+        JsonSerializerSettings settings;
+        [SetUp]
+        public void Init()
         {
-            var settings = new JsonSerializerSettings();
+            settings = new JsonSerializerSettings();
             JsonConvert.DefaultSettings = () => settings;
 
             settings.Converters.Add(JsonSubtypesConverterBuilder
                 .Of(typeof(Payload), Payload.PAYLOAD_KIND)
-                .SerializeDiscriminatorProperty()
                 .RegisterSubtype(typeof(Game), PayloadDiscriminator.GAME)
                 .RegisterSubtype(typeof(Com), PayloadDiscriminator.COM)
                 .Build());
 
             settings.Converters.Add(JsonSubtypesConverterBuilder
                 .Of(typeof(Game), Game.GAME_KIND)
-                .SerializeDiscriminatorProperty()
                 .RegisterSubtype(typeof(Walk), GameDiscriminator.WALK)
                 .RegisterSubtype(typeof(Run), GameDiscriminator.RUN)
                 .Build());
-            
+        }
+
+        [Test]
+        public void ShouldDeserializeNestedLevel()
+        {
+            var data = "{\"$GameKind\":0,\"$PayloadKind\":1}";
+            Assert.IsInstanceOf<Run>(JsonConvert.DeserializeObject<Payload>(data, settings));
+        }
+
+        [Test]
+        public void ShouldSerializeNestedLevel()
+        {
             Payload run = new Run();
             var data = JsonConvert.SerializeObject(run, settings);
-            Assert.IsInstanceOf<Run>(JsonConvert.DeserializeObject<Payload>(data, settings));
+            Assert.AreEqual("{\"$GameKind\":0,\"$PayloadKind\":1}", data);
         }
 
         public enum PayloadDiscriminator
@@ -108,9 +123,6 @@ namespace JsonSubTypes.Tests
             WALK = 1
         }
 
-        [JsonConverter(typeof(JsonSubtypes), PAYLOAD_KIND)]
-        [JsonSubtypes.KnownSubType(typeof(Game), PayloadDiscriminator.GAME)]
-        [JsonSubtypes.KnownSubType(typeof(Com), PayloadDiscriminator.COM)]
         public abstract class Payload
         {
             public const string PAYLOAD_KIND = "$PayloadKind";
@@ -119,9 +131,6 @@ namespace JsonSubTypes.Tests
             protected abstract PayloadDiscriminator PayloadKind { get; }
         }
 
-        [JsonConverter(typeof(JsonSubtypes), GAME_KIND)]
-        [JsonSubtypes.KnownSubType(typeof(Run), GameDiscriminator.RUN)]
-        [JsonSubtypes.KnownSubType(typeof(Walk), GameDiscriminator.WALK)]
         public abstract class Game : Payload
         {
             protected override PayloadDiscriminator PayloadKind => PayloadDiscriminator.GAME;
@@ -145,6 +154,83 @@ namespace JsonSubTypes.Tests
         public class Run : Game
         {
             protected override GameDiscriminator GameKind => GameDiscriminator.RUN;
+        }
+    }
+
+    [TestFixture]
+    public class MultipleHierarchyLevelsDynamicRegistrationWriteDiscriminatorTests
+    {
+        JsonSerializerSettings settings;
+        [SetUp]
+        public void Init()
+        {
+            settings = new JsonSerializerSettings();
+            JsonConvert.DefaultSettings = () => settings;
+
+            settings.Converters.Add(JsonSubtypesConverterBuilder
+                .Of(typeof(Payload), Payload.PAYLOAD_KIND)
+                .SerializeDiscriminatorProperty()
+                .RegisterSubtype(typeof(Game), PayloadDiscriminator.GAME)
+                .RegisterSubtype(typeof(Com), PayloadDiscriminator.COM)
+                .Build());
+
+            settings.Converters.Add(JsonSubtypesConverterBuilder
+                .Of(typeof(Game), Game.GAME_KIND)
+                .SerializeDiscriminatorProperty()
+                .RegisterSubtype(typeof(Walk), GameDiscriminator.WALK)
+                .RegisterSubtype(typeof(Run), GameDiscriminator.RUN)
+                .Build());
+        }
+
+        [Test]
+        public void ShouldDeserializeNestedLevel()
+        {
+            var data = "{\"$GameKind\":0,\"$PayloadKind\":1}";
+            Assert.IsInstanceOf<Run>(JsonConvert.DeserializeObject<Payload>(data, settings));
+        }
+
+        [Test]
+        [Ignore("Not supported: not implemented")]
+        public void ShouldSerializeNestedLevel()
+        {
+            Payload run = new Run();
+            var data = JsonConvert.SerializeObject(run, settings);
+            Assert.AreEqual("{\"$GameKind\":0,\"$PayloadKind\":1}", data);
+        }
+
+        public enum PayloadDiscriminator
+        {
+            COM = 0,
+            GAME = 1
+        }
+
+        public enum GameDiscriminator
+        {
+            RUN = 0,
+            WALK = 1
+        }
+
+        public abstract class Payload
+        {
+            public const string PAYLOAD_KIND = "$PayloadKind";
+        }
+
+        public abstract class Game : Payload
+        {
+            public const string GAME_KIND = "$GameKind";
+
+        }
+
+        public class Com : Payload
+        {
+        }
+
+        public class Walk : Game
+        {
+        }
+
+        public class Run : Game
+        {
         }
     }
 }
