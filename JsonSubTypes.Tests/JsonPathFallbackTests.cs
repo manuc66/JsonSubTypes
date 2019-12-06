609af9ea342ac72f01167ad4aac94d2b0373b717
remgodow <rg1993@o2.pl>
2019-12-06 15:43:21 +0100

    Added discriminator tests.

--- JsonSubTypes.Tests/JsonPathFallbackTests.cs
+++ JsonSubTypes.Tests/JsonPathFallbackTests.cs
@@ -28,6 +28,55 @@ namespace JsonSubTypes.Tests.JsonPath
         string property { get; set; }
     }
 
+
+    class NestedClass
+    {
+
+    }
+
+    [JsonConverter(typeof(JsonSubtypes), "nested.property")]
+    [JsonSubtypes.KnownSubType(typeof(SubDiscriminator), "SubNestedClass")]
+    [JsonSubtypes.KnownSubType(typeof(OtherDiscriminator), "OtherNestedClass")]
+    class MainDiscriminator
+    {
+        public NestedClass nested;
+
+    }
+
+    class SubNestedClass: NestedClass
+    {
+        string property = "SubNestedClass"; 
+    }
+
+    class OtherNestedClass: NestedClass
+    {
+        string property = "OtherNestedClass";
+    }
+
+    class SubDiscriminator : MainDiscriminator
+    {
+        new SubNestedClass nested { get; set; }
+    }
+
+    class OtherDiscriminator : MainDiscriminator
+    {
+        new OtherNestedClass nested { get; set; }
+    }
+
+
+    [JsonConverter(typeof(JsonSubtypes), "dotted.property")]
+    [JsonSubtypes.KnownSubType(typeof(SubDottedDiscriminator), "SubNestedClass")]
+    class MainDottedDiscriminator
+    {
+        [JsonProperty("dotted.property")]
+        string property { get; set; }
+    }
+
+    class SubDottedDiscriminator: MainDottedDiscriminator
+    {
+        
+    }
+
     [TestFixture]
     public class JsonPathFallbackTests
     {
@@ -40,11 +89,27 @@ namespace JsonSubTypes.Tests.JsonPath
         }
 
         [Test]
-        public void CheckJsonPathFallback()
+        public void CheckNestedProperty()
         {
             string json = "{nested: { otherproperty: \"abc\" } }";
             var result = JsonConvert.DeserializeObject<Main>(json);
             Assert.IsInstanceOf<Nested>(result);
+        }
+
+        [Test]
+        public void CheckNestedDiscriminator()
+        {
+            string json = "{nested: { property: \"SubNestedClass\" } }";
+            var result = JsonConvert.DeserializeObject<MainDiscriminator>(json);
+            Assert.IsInstanceOf<SubDiscriminator>(result);
+        }
+
+        [Test]
+        public void CheckDottedDiscriminator()
+        {
+            string json = "{\"dotted.property\": \"SubNestedClass\"}";
+            var result = JsonConvert.DeserializeObject<MainDottedDiscriminator>(json);
+            Assert.IsInstanceOf<SubDottedDiscriminator>(result);
         }
     }
 }
