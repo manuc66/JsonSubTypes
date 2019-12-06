3848ad418ad496396a5ff887f7d40e33bb6ac04b
remgodow <rg1993@o2.pl>
2019-12-06 15:26:52 +0100

    Added tests for dotted property and JsonPath.

---
+++ JsonSubTypes.Tests/JsonPathFallbackTests.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace JsonSubTypes.Tests.JsonPath
+{
+    class Nested: Main
+    {
+        string otherproperty { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonSubtypes))]
+    [JsonSubtypes.KnownSubTypeWithProperty(typeof(DottedSub), "nested.property")]
+    [JsonSubtypes.KnownSubTypeWithProperty(typeof(Nested), "nested.otherproperty")]
+
+    class Main
+    {
+
+    }
+
+    class Sub : Main
+    {
+        Nested nested { get; set; }
+    }
+
+    class DottedSub : Main
+    {
+        [JsonProperty("nested.property")]
+        string property { get; set; }
+    }
+
+    [TestFixture]
+    public class JsonPathFallbackTests
+    {
+        [Test]
+        public void CheckDottedProperty()
+        {
+            string json = "{\"nested.property\": \"str\"}";
+            var result = JsonConvert.DeserializeObject<Main>(json);
+            Assert.IsInstanceOf<DottedSub>(result);
+        }
+
+        [Test]
+        public void CheckJsonPathFallback()
+        {
+            string json = "{nested: { otherproperty: \"abc\" } }";
+            var result = JsonConvert.DeserializeObject<Main>(json);
+            Assert.IsInstanceOf<Nested>(result);
+        }
+    }
+}
