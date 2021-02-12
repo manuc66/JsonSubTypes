ee8d5e3537d511b41a5de0fcbf4e5e0b2b551482
markus.buschhoff <info@buschhoff-medien.de>
2021-02-12 11:45:37 +0100

    added support for cross-assembly discriminator properties that use fully qualified names

--- JsonSubTypes/JsonSubtypes.cs
+++ JsonSubTypes/JsonSubtypes.cs
@@ -380,6 +380,11 @@ namespace JsonSubTypes
                 var searchLocation = parentTypeFullName.Substring(0, parentTypeFullName.Length - parentType.Name.Length);
                 typeByName = insideAssembly.GetType(searchLocation + typeName, false, true);
             }
+            
+            if (parentTypeFullName != null && typeByName == null)
+            {
+                typeByName = Type.GetType(typeName);
+            }
 
             var typeByNameInfo = ToTypeInfo(typeByName);
             if (typeByNameInfo != null && parentType.IsAssignableFrom(typeByNameInfo))
