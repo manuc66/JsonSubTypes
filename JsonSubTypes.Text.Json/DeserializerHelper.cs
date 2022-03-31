ea5e88b19fc5ea4adc18b2b58d7c6987568b14b2
manuc66 <manuc66@gmail.com>
2022-03-31 23:47:57 +0200

    fix compilation

--- JsonSubTypes.Text.Json/DeserializerHelper.cs
+++ JsonSubTypes.Text.Json/DeserializerHelper.cs
@@ -4,7 +4,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text.Json;
 
-namespace NewApi;
+namespace JsonSubTypes.Text.Json;
 
 internal class DeserializerHelper<T>
 {
--- JsonSubTypes.Text.Json/JsonSubtypes2.cs
+++ JsonSubTypes.Text.Json/JsonSubtypes2.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
-using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
